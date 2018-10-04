3e4c223ced9b498e77e9eda802a4ce7bca54c00b
Tomazicon <dave.tomazin@gmail.com>
2018-10-04 12:09:53 -0600

    Updates

---
+++ precache-manifest.74b34acfc93502e0a3b13dda9cf00967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marvelclick/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8be61d7211caa8132885",
+    "url": "/marvelclick/static/js/runtime~main.8be61d72.js"
+  },
+  {
+    "revision": "7f5807f2f159a2113d10",
+    "url": "/marvelclick/static/js/main.7f5807f2.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/marvelclick/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "7f5807f2f159a2113d10",
+    "url": "/marvelclick/static/css/main.9a1fba83.chunk.css"
+  },
+  {
+    "revision": "bfc8b17cd14c5d2738be0a4b50d280e8",
+    "url": "/marvelclick/index.html"
+  }
+];
